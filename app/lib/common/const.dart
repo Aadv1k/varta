b6d4ee967040aa59e84a147f3c137350126858a7
Aadvik <aadv1k@outlook.com>
2024-11-27 16:30:47 +0530

    The gut feeling was correct - I indeed messed up by API url being `http` and not `https`

--- app/lib/common/const.dart
+++ app/lib/common/const.dart
@@ -2,7 +2,7 @@ const int maxUploadSizeInBytes = 1024 * 1024 * 100;
 const int maxAttachmentCount = 12;
 const int maxQuotaForAttachmentsInBytes = 1024 * 1024 * 150;
 
-const String baseApiUrl = "http://varta.aadvikpandey.com/api/v1";
+const String baseApiUrl = "https://varta.aadvikpandey.com/api/v1";
 
 const int pollingDurationInSeconds = 5;
 
